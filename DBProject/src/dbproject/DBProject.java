834c01b60ada07fc7a87438e4ee8dc9aaedb1174
Krongrah <Krongrah@gmail.com>
2018-03-02 10:42:38 +0100

    moved a file

--- DBProject/src/dbproject/DBProject.java
+++ DBProject/src/dbproject/DBProject.java
@@ -5,6 +5,13 @@
  */
 package dbproject;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
 /**
  *
  * @author Krongrah
@@ -16,6 +23,61 @@ public class DBProject {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    }
+    
+    
+        void connectToDatabase(){
+    try {
+            Class.forName("org.postgresql.Driver");
+        } catch (java.lang.ClassNotFoundException e) {
+            System.out.println(e);
+        }
+
+
+        String url = "jdbc:postgresql://horton.elephantsql.com:5432/baasu";
+        String username = "tilfjjea";
+        String password = "tC0sMMGx8EH6KyQ7CRBVEIUVzC82C8Zv";
+
+
+
+
+
+        try (Connection con = DriverManager.getConnection(url, username, password)){
+            
+
+            Statement statement = con.createStatement();
+            ResultSet rs = statement.executeQuery("select * from test3");
+            while (rs.next()) {
+
+                System.out.print(rs.getString(1) + " ");
+                System.out.println(rs.getString(2) + " ");
+            }
+ 
+
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+    
+    
+    List first(){
+    
+    return null;
+    }
+    
+    List second(){
+    
+    return null;
+    }
+    
+    List third(){
+    
+    return null;
+    }
+    
+    List fourth(int number){
+        
+    return null;
     }
     
 }
